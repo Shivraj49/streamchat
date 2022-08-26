1a44d02c4d1d7d464cfebcb0fcce0321ecbdfd0e
shivraj <shivrajkolpe2997@gmail.com>
2022-08-26 15:12:10 +0530

    commit

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ server_client = stream_chat.StreamChat(api_key="d3es7d8cqtjj", api_secret="6njpt
 
 class Parameters(BaseModel):
     user_name: str
+    id:str
 
 @app.post("/token")
 def getPreferences(parameter: Parameters):
-    token = server_client.create_token(parameter.user_name)
+    server_client.upsert_user({"id": parameter.id, "name": parameter.user_name})
+    token = server_client.create_token(parameter.id)
     return {"token": token}
